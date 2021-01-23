b92ee7961c780714e5f537f645bfc60dfeb0f0d9
yw9594 <yw9594@naver.com>
2021-01-24 02:57:31 +0900

    2021.01.24 BOJ_1916 updated

--- codes/codes/BOJ_1916_using_heap.cpp
+++ codes/codes/BOJ_1916_using_heap.cpp
@@ -8,24 +8,25 @@
 
 using namespace std;
 
-typedef pair<int, int> Pos;
 
 /////////////////////////////////////////////////// 공통 Header ///////////////////////////////////////////////////
-#define INF 987654321
+#define INF 9876543210
 
-int N, M;
-int start, dest;
-vector<long long int> dist;
-vector<vector<long long int>> weight;
-vector<vector<int>> matrix;
+using LL = long long int;
+typedef pair<int, LL> Pos;
 
 struct Elem {
-	int n;					// 노드 번호
-	long long int w;		// 경로 가중치 합
-	bool operator<(const Elem& arg) const {	// 경로에 포함되지 않은 노드 중 가장 경로합이 작은 노드
+	int n;
+	LL w; // 노드 번호, 경로 합
+	bool operator<(const Elem& arg) const {
 		return this->w > arg.w;
 	}
 };
+
+int N, M;
+int start, dest;
+vector<LL> dist;
+vector<vector<Pos>> matrix;
 
 int main(void) {
 	freopen("input.txt", "r", stdin);
@@ -33,47 +34,44 @@ int main(void) {
 	cin.tie(NULL);
 
 	cin >> N >> M;
+	dist = vector<LL>(N + 1, INF); // 가중치를 포함하는 인접리스트 
+	matrix = vector<vector<Pos>>(N + 1, vector<Pos>()); // 가중치를 포함하는 인접리스트 
 
-	dist = vector<long long int>(N + 1, INF);
-	weight = vector<vector<long long int>>(N + 1, vector<long long int>(N + 1, INF));	// 가중치 배열 초기화
-	matrix = vector<vector<int>>(N + 1, vector<int>()); // 인접 리스트 초기화
-
-	// 입력 받기
-	for (int i = 1; i <= M; i++) {
+	// 입력
+	while (M--) {
 		int s, e, w;
 		cin >> s >> e >> w;
-		if (w < weight[s][e]) {
-			weight[s][e] = w;		// 가중치 배열
-			matrix[s].push_back(e); // 인접 리스트 
-		}
+		matrix[s].push_back({ e, w });
 	}
-	cin >> start >> dest; // 출발지/목적지
+	cin >> start >> dest;
 
 
-	// 다익스트라 수행
+	// 다익스트라
 	priority_queue<Elem> pq;
-	pq.push({ start, 0 });
+	pq.push({ start, 0 }); // 시작노드는 경로집합에 포함한다.
+	dist[start] = 0;
 
 	while (!pq.empty()) {
-		Elem cur_node = pq.top(); // 경로 집합에 포함되지 않은 노드 중, 가중합이 가장 작은 노드를 포함시킬 것이다.
+		Elem cur_node = pq.top();
 		pq.pop();
 
-		if (dist[cur_node.n] < cur_node.w) // 가고자하는 노드의 경로 가중합이 찾은 경로의 가중합보다 더 큰 경우, 연산을 수행하지 않는다. 
+		// 현재 노드보다 이미 찾은 경로합이 더 작은 경우 현재 경로는 무시한다. 
+		if (dist[cur_node.n] < cur_node.w)
 			continue;
 
-		dist[cur_node.n] = cur_node.w; // 포함시킬 노드의 경로 합을 수정한다.				
-
-
+		// 경로집합에 포함되지 않은 원소들 중에서 가중합이 더 작아지는 노드들을 골라 넣는다.
 		for (auto& e : matrix[cur_node.n]) {
-			long long int new_dist = dist[cur_node.n] + weight[cur_node.n][e];// 현재 노드가 경로 집합에 포함됨에 따라 추가되는 갈 수 있는 노드들의 경로 합
-			if (new_dist < dist[e]) { // 새로이 포함된 노드에서 다음 노드로 갈 경로 중 더 빠른 노드가 존재한다면 큐에 포함한다.
-				pq.push({ e, new_dist });
+			LL new_weight = dist[cur_node.n] + e.second; // 현재 노드를 거쳐 노드 e로 가는 경로 가중 합
+			if (new_weight < dist[e.first]) {
+				pq.push({ e.first, new_weight });
+				dist[e.first] = new_weight;
 			}
 		}
-	}
-	//show_1d_array(dist);
-	cout << dist[dest] << "\n";
 
+	}
+
+
+	cout << dist[dest] << "\n";
 
 	return 0;
 }*/
