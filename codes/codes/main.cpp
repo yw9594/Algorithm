a6d3309c6a37b783edbdd913e92e3291c2195d65
yw9594 <yw9594@naver.com>
2021-03-21 02:27:00 +0900

    2021.03.21 string practice

--- codes/codes/main.cpp
+++ codes/codes/main.cpp
@@ -23,39 +23,13 @@ cin.tie(NULL);
 */
 
 #include <iostream>
-#include <algorithm>
 #include <vector>
 #include <string>
-#include <cctype>
-#include <sstream>
-
 using namespace std;
-
-vector<string> split(string& str, char delim=' ') {
-	vector<string> toks;
-	stringstream ss(str);
-	string tok;
-
-	while (getline(ss, tok, delim)) toks.push_back(tok);
-
-	return toks;
-}
 
 int main(void) {
 	// freopen("input.txt", "r", stdin);
-	vector<string> toks;
 
-	string str1= "1 2 3 4 5";
-	string str2 = "1n2n3n4n";
 
-	toks = split(str1);
-	for (auto& e : toks) cout << e << " ";
-	cout << "\n";
-
-	toks = split(str1, 'n');
-	for (auto& e : toks) cout << e << " ";
-	cout << "\n";
-
-	
 	return 0;
 }
---
+++ codes/codes/practice_string_find.cpp
@@ -0,0 +1,30 @@
+/* 
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main(void) {
+	// freopen("input.txt", "r", stdin);
+
+	// 1. 단일 문자 검색
+	char c = '@';
+	cout << strchr("!@#$%", c) << "\n"; // 없으면 0, 있으면 주소
+
+	// 2. 문자열 검색
+	string str = "abcabcabcabcabc";
+	string sub = "abc";
+	vector<int> arr;
+
+	int idx = 0;
+	while ((idx = str.find(sub, idx)) != -1) {
+		idx += sub.size();
+		arr.push_back(idx);
+	}
+	for (auto& i : arr) cout << i << " ";
+	cout << "\n";
+
+
+
+	return 0;
+}*/
