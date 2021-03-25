135526928dfcdf102a501199df5cd73becd3497c
Loge <yw9594@naver.com>
2021-03-26 01:36:34 +0900

    2021.03.26 string & trie

---
+++ codes/PG/PG_가사 검색.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+
+#include <algorithm>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <list>
+#include <map>
+#include <set>
+
+#include <string>
+#include <cstring>
+#include <sstream>   // ex) stringstream
+
+#include <cmath>    
+#include <cctype>    // ex) isdigit..
+
+#include "infos.h"
+
+using namespace std;
+
+// 프로그래머스 사용 시 전역 변수는 반드시 solution 내부에서 초기화 할 것.
+struct Node{
+	int flag, cnt;
+	Node * child[26];
+	Node() {
+		cnt=0;
+		flag = false;
+		for(auto & e : child) e=NULL;
+	}
+};
+struct Tree {
+	Node*root;
+	Tree(){	root = new Node();}
+	int charToInt(char & c){return c-'a';}
+	void push(string & word, int is_reverse){
+		int idx = is_reverse > 0 ? 0 : word.size()-1, 
+			lim = is_reverse > 0 ? word.size() : -1;
+		Node * cur_node = root;	
+		while(idx!=lim) {
+			int c = charToInt(word[idx]);
+			if(cur_node->child[c] == NULL) cur_node->child[c] = new Node();
+			cur_node->cnt++;
+			cur_node =  (cur_node->child)[c];
+			idx += is_reverse;
+		}
+		cur_node->flag = true;
+	}
+	int find(string & query){
+		int cnt=0;
+		findRecursive(query, root, cnt, 0);
+		return cnt;
+	}
+	void findRecursive(string & query, Node * cur_node, int & cnt, int idx) {
+		if(idx==query.size()){
+			if(cur_node->flag)
+				cnt++;
+		}
+		else if(query[idx]=='?') {
+			cnt+=cur_node->cnt;
+		}
+		else{
+			int c = charToInt(query[idx]);
+			if((cur_node->child)[c]!=NULL)
+				findRecursive(query, (cur_node->child)[c], cnt, idx+1);
+		}
+	}
+
+};
+
+
+vector<int> solution(vector<string> words, vector<string> queries) {
+	vector<int> answer;
+	Tree tree[10001], rtree[10001];
+
+	for(auto & w : words) {
+		tree[w.size()].push(w, 1);
+		rtree[w.size()].push(w, -1);
+	}
+
+	for(auto & q : queries) {
+		int cnt;
+		if(q[0]=='?'){
+			reverse(q.begin(), q.end());
+			cnt = rtree[q.size()].find(q);
+			reverse(q.begin(), q.end());
+		}
+		else{
+			cnt = tree[q.size()].find(q);
+		}
+		answer.push_back(cnt);
+	}
+
+	return answer;
+}
+
+int main(void) {
+	// input
+	/*freopen("input.txt", "r", stdin);
+	std::istream>::sync_with_stdio(false);
+	cin.tie(NULL);
+	*/
+	vector<string> words = {"frodo", "front", "frost", "frozen", "frame", "kakao"};
+	vector<string> queries = {"fro??", "????o", "fr???", "fro???", "pro?"};
+
+	showLine("start");
+	vector<int> ans = solution(words, queries);
+	show1dArray(ans);
+
+	showLine("done");
+	return 0;
+}
--- codes/main.cpp
+++ codes/main.cpp
@@ -20,60 +20,6 @@
 using namespace std;
 
 // 프로그래머스 사용 시 전역 변수는 반드시 solution 내부에서 초기화 할 것.
-struct elem{
-	int s,b,o;
-};
-
-vector<elem> tokenize(string & log){
-	int idx=0, n=log.size();
-	int s, b,o;
-	vector<elem> ret;
-	while(idx<n){
-		if(isdigit(log[idx])) {
-			if(idx!=0 && isdigit(log[idx-1])) s = s * 10 + (log[idx]-'0');
-			else s = (log[idx]-'0');
-		}
-		else if(strchr("SDT", log[idx])){
-			if(log[idx]=='S') b=1;
-			else if(log[idx]=='D') b=2;
-			else b=3;
-			
-			if(idx+1<n && strchr("*#", log[idx+1])){
-				idx++;
-				o = (log[idx]=='*'? 2 : -1);
-			}
-			else o=1;
-			ret.push_back({s, b, o});
-		}
-		idx++;
-	}
-	return ret;
-}
-
-inline int calcScore(elem & e) {
-	int s = 1;
-	for(int i=0; i<e.b; i++) s *= e.s;
-	return s * e.o;
-}
-
-int solution(string dartResult) {
-    int answer = 0;
-	vector<elem> tokens = tokenize(dartResult);
-	
-	int old_score= calcScore(tokens[0]);
-
-	for(int i=1; i<tokens.size(); i++){
-		elem & cur_token = tokens[i];
-		int cur_score = calcScore(cur_token);
-
-		if(cur_token.o==2) old_score*=2;
-		answer+= old_score;
-		old_score = cur_score;
-	}
-	answer += old_score;
-
-    return answer;
-}
 
 int main(void) {
 	// input
@@ -83,10 +29,7 @@ int main(void) {
 	*/
 
 	showLine("start");
-	
-	cout << solution("1S2D*3T")<<"\n";
-	cout << solution("1D2S#10S")<<"\n";
-	cout << solution("1S*2T*3S")<<"\n";
+	show1dArray(ans);
 
 	showLine("done");
 	return 0;
